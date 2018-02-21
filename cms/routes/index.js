1a06a683310bf08471d8c4f7b065f7a7cdff181f
oshige <oshige@mono-96.jp>
2018-02-21 11:41:52 +0900

    fix lint cms/routes/

--- cms/routes/index.js
+++ cms/routes/index.js
@@ -18,29 +18,28 @@
  * http://expressjs.com/api.html#app.VERB
  */
 
-var keystone = require('keystone');
-var middleware = require('./middleware');
-var importRoutes = keystone.importer(__dirname);
+var keystone = require('keystone')
+var middleware = require('./middleware')
+var importRoutes = keystone.importer(__dirname)
 
 // Common Middleware
-keystone.pre('routes', middleware.initLocals);
-keystone.pre('render', middleware.flashMessages);
+keystone.pre('routes', middleware.initLocals)
+keystone.pre('render', middleware.flashMessages)
 
 // Import Route Controllers
 var routes = {
-	views: importRoutes('./views'),
-};
+  views: importRoutes('./views')
+}
 
 // Setup Route Bindings
 exports = module.exports = function (app) {
-	// Views
-	app.get('/', routes.views.index);
-	app.get('/blog/:category?', routes.views.blog);
-	app.get('/blog/post/:post', routes.views.post);
-	app.get('/gallery', routes.views.gallery);
-	app.all('/contact', routes.views.contact);
+  // Views
+  app.get('/', routes.views.index)
+  app.get('/blog/:category?', routes.views.blog)
+  app.get('/blog/post/:post', routes.views.post)
+  app.get('/gallery', routes.views.gallery)
+  app.all('/contact', routes.views.contact)
 
-	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
-	// app.get('/protected', middleware.requireUser, routes.views.protected);
-
-};
+  // NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
+  // app.get('/protected', middleware.requireUser, routes.views.protected);
+}
